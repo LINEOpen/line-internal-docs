70b2974f9d552d01b7fca604dc1737baf4352e7c
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-29 21:12:30 +0900

    displayAllHeaders: true

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       { text: 'Home', link: '/' },
       { text: 'Introduction', link: '/docs' }
     ],
-
+    displayAllHeaders: true,
     sidebar: [
       {
         
