11bcd00667d6d2ae4ce97968d71e030010b0766d
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-29 21:16:06 +0900

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,5 +1,19 @@
 import { defineConfig } from 'vitepress'
-
+import fs from "fs"
+import path from "path"
+var dirpath = "./docs"
+var dirs = fs.readdirSync(dirpath).filter((f) => {
+  return fs.existsSync(dirpath + "/" + f) && fs.statSync(dirpath + "/" + f).isDirectory()
+})
+var sidebarArray = ["/"].concat(dirs.map((dir) => {
+  return {
+    title: dir,
+    collapsable: true,
+    children: fs.readdirSync(dirpath + "/" + dir).map((childDir) => {
+      return dirpath + "/" + dir + "/" + childDir
+    })
+  }
+}))
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "LINE schemas project",
@@ -12,12 +26,7 @@ export default defineConfig({
       { text: 'Introduction', link: '/docs' }
     ],
     displayAllHeaders: true,
-    sidebar: [
-      {
-        
-      }
-    ],
-
+    sidebar: sidebarArray,
     socialLinks: [
       { icon: 'github', link: 'https://github.com/nakasyou/LINE-schemas' }
     ]
