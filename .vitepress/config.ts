657efa0dc864ac4d2869744599dbb8ed9aa7483e
nakasyou <nakasyou1103@gmail.com>
2023-04-30 02:59:12 +0000

    add auto sidebar

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vitepress'
-import fs from "fs"
-import path from "path"
-var dirpath = "./docs"
-var dirs = fs.readdirSync(dirpath).filter((f) => {
-  return fs.existsSync(dirpath + "/" + f) && fs.statSync(dirpath + "/" + f).isDirectory()
-})
-var sidebarArray = ["/"].concat(dirs.map((dir) => {
-  return {
-    title: dir,
-    collapsable: true,
-    children: fs.readdirSync(dirpath + "/" + dir).map((childDir) => {
-      return dirpath + "/" + dir + "/" + childDir
-    })
-  }
-}))
+
+import sidebar from "vuepress-auto-sidebar"
+
+sidebar.baseOption = "./docs"
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "LINE schemas project",
@@ -26,9 +16,10 @@ export default defineConfig({
       { text: 'Introduction', link: '/docs' }
     ],
     displayAllHeaders: true,
-    sidebar: sidebarArray,
+    sidebar: sidebar.getSidebar(),
     socialLinks: [
       { icon: 'github', link: 'https://github.com/nakasyou/LINE-schemas' }
     ]
   }
 })
+
