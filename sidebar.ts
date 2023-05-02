9667323d81e0d5e0fd9d9ed9e7a5a3a727825fe7
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-05-02 21:14:30 +0900

    Update sidebar.ts

--- sidebar.ts
+++ sidebar.ts
@@ -3,7 +3,7 @@ import fs from "fs"
 function getFiles(path){
   return fs.readdirSync(path, { withFileTypes: true }).filter(dirent => dirent.isFile()).map(e=>path+e.name);
 }
-const getSchemes = (path) => getFiles(path).map(path => {
+const getSchemes = (path, noth2: boolean | void) => getFiles(path).map(path => {
   const markdown = fs.readFileSync(path, 'utf8');
   let h1 = markdown.match(/^# .+$/mg);
   h1 = h1 ? h1[0].slice(2) : "None";
@@ -11,6 +11,12 @@ const getSchemes = (path) => getFiles(path).map(path => {
     const h2s = markdown.match(/^## .+$/mg);
     return h2s ? h2s.map(h2=>h2.slice(3)) : [];
   })();
+  if(noth2){
+    return {
+      text: h1,
+      link: path.replace(/(\.md)|(\.)/g,""),
+    }
+  }
   return {
     text: h1,
     items: h2s.map(h2=>({
@@ -24,7 +30,7 @@ const getSchemes = (path) => getFiles(path).map(path => {
 export default () => ([
   { text: "Introduction", link: "/docs" },
   { text: "ルール", link: "/docs/rule" },
-  { text: "Words", link: "/docs/words", items: getSchemes("./docs/words/"), collapsed: false,},
+  { text: "Words", link: "/docs/words", items: getSchemes("./docs/words/",true), collapsed: false,},
   {
     text: "Schemas",
     items: getSchemes("./docs/schemes/"),
